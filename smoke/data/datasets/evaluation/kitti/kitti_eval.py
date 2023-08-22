4ce294616ad55db4caf3b513769be53f763dad0f
soe <buliyanran@gmail.com>
2023-08-23 00:17:02 +0800

    changes

--- smoke/data/datasets/evaluation/kitti/kitti_eval.py
+++ smoke/data/datasets/evaluation/kitti/kitti_eval.py
@@ -47,9 +47,9 @@ def do_kitti_detection_evaluation(dataset,
     output_dir = os.path.abspath(output_folder)
     os.chdir('./smoke/data/datasets/evaluation/kitti/kitti_eval')
     label_dir = getattr(dataset, 'label_dir')
-    if not os.path.isfile('evaluate_object_3d_offline'):
-        subprocess.Popen('g++ -O3 -DNDEBUG -o evaluate_object_3d_offline evaluate_object_3d_offline.cpp', shell=True)
-    command = "./evaluate_object_3d_offline {} {}".format(label_dir, output_dir)
+    if not os.path.isfile('/soe/SMOKE/evaluate_object_3d_offline'):
+        subprocess.Popen('g++ -O3 -DNDEBUG -o /soe/SMOKE/evaluate_object_3d_offline /soe/SMOKE/evaluate_object_3d_offline.cpp', shell=True)
+    command = "/soe/SMOKE/evaluate_object_3d_offline {} {}".format(label_dir, output_dir)
     output = subprocess.check_output(command, shell=True, universal_newlines=True).strip()
     logger.info(output)
     os.chdir('./tools')
