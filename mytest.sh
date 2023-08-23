442f1edbefbb785754e78908b694eb6a1d5bd25c
soe <buliyanran@gmail.com>
2023-08-24 02:36:07 +0800

    change

--- mytest.sh
+++ mytest.sh
@@ -5,3 +5,7 @@ mv evaluate_object_3d_offline.cpp smoke/data/datasets/evaluation/kitti/kitti_eva
 python tools/plain_train_net.py --eval-only --config-file "configs/smoke_gn_vector.yaml"
 
 # Most config files are under /smoke/config/defaults.py
+
+
+# to change the backbone to be Mobile Net the fellowing should be down:
+#1. /smoke/configure/defaults.py line 76 _C.MODEL.BACKBONE.CONV_BODY = "MobileNetV2" #"DLA-34-DCN"
