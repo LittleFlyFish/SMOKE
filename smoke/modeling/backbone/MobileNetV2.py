42f805327993c8974d2cc3415b8f0c38d1c8c6dd
soe <buliyanran@gmail.com>
2023-09-05 20:44:41 +0800

    change

--- smoke/modeling/backbone/MobileNetV2.py
+++ smoke/modeling/backbone/MobileNetV2.py
@@ -71,8 +71,8 @@ class MobileNetV2(nn.Module):
             # t, c, n, s
             [1, 16, 2, 1], # increase n won't affect the output size of img
             [6, 64, 3, 2], # increase n won't affect the output size of img
-            [6, 64, 4, 1], #
-            [6, 64, 2, 1]
+            [6, 64, 4, 1], # increase n won't affect the output size of img
+            [6, 64, 3, 1]
         ]
 
         # # The initial mobileNet config
