a88f59c4bee5a013f58f126a2efa21868f565082
soe <buliyanran@gmail.com>
2023-09-13 16:40:14 +0800

    change

--- tools/visualization.py
+++ tools/visualization.py
@@ -239,8 +239,8 @@ def show_image_with_boxes(save_folder, img, objects, calib, count=0):
 
 
 if __name__ == "__main__":
-    save_path = '/mount/data/Documents/kitti/Results'
-    root_dir = '/home/soe/Documents/kitti' # training and testing are under it
+    save_path = '/media/data1/yanran/SMOKE/datasets/kitti/Results' # '/mount/data/Documents/kitti/Results'
+    root_dir = '/media/data1/yanran/SMOKE/datasets/kitti' # '/home/soe/Documents/kitti' # training and testing are under it
 
     set_label = ['training', 'testing']
     split_set = set_label[1]
