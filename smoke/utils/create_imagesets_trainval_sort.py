d33c8dfa009b3cbbd9770172f4b41edd95ac936f
soe <buliyanran@gmail.com>
2023-09-18 14:57:37 +0800

    change

--- smoke/utils/create_imagesets_trainval_sort.py
+++ smoke/utils/create_imagesets_trainval_sort.py
@@ -1,11 +1,12 @@
 import os
-files = os.listdir('/home/soe/Documents/kitti/testing/image_2')
+files = os.listdir('./image_2')
 files.sort()
 
-save_txt = open('/home/soe/Documents/kitti/testing/ImageSets/test.txt', 'w')
+save_txt = open('./ImageSets/trainval.txt', 'w')
 
 for file in files:
     print('file:', file)
     file_name = file.split('.')[0]
     save_txt.write(file_name)
     save_txt.write('\n')
+    
