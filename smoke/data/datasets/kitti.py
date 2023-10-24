fa10fe7326bbd709c9814f6f0eab7dab2b21ba79
soe <buliyanran@gmail.com>
2023-10-24 18:50:23 +0100

    changes

--- smoke/data/datasets/kitti.py
+++ smoke/data/datasets/kitti.py
@@ -231,6 +231,7 @@ class KITTIDataset(Dataset):
             for line, row in enumerate(reader):
                 if row[0] == 'P2:':
                     K = row[1:]
+                    print(K)
                     K = [float(i) for i in K]
                     K = np.array(K, dtype=np.float32).reshape(3, 4)
                     K = K[:3, :3]
